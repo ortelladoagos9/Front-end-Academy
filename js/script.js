ba504dea87f2ee1bc53f452065ad5f56d1eb788d
agos ortellado <ortelladoagos@gmail.com>
2023-02-28 17:31:19 -0300

    proyecto terminado

---
+++ js/script.js
@@ -0,0 +1,8 @@
+document.getElementById("btn-send").addEventListener("click", function () {
+   
+    document.getElementById("send-message").innerHTML = "Thank you! I remain attentive to your message, soon you will have an answer :)";
+    document.getElementById("form").style.display = "none";
+    
+})
+
+
